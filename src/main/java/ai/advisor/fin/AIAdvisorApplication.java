ba6ea464bb8af9d4fcadc3c9068c0e59ed44940c
Rohit Singh <rohit.singh@ing.com>
2024-07-22 11:05:17 +0200

    Added template for application

---
+++ src/main/java/ai/advisor/fin/AIAdvisorApplication.java
@@ -0,0 +1,12 @@
+package ai.advisor.fin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AIAdvisorApplication.class, args);
+    }
+}
